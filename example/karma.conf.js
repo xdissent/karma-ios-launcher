9c0be1aed2a53089fcdf10431bc740c945920041
Greg Thornton <xdissent@me.com>
2013-12-09 15:42:53 -0600

    Add example

---
+++ example/karma.conf.js
@@ -0,0 +1,79 @@
+// Karma configuration
+// Generated on Fri Dec 06 2013 02:01:17 GMT-0600 (CST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['iOS'],
+
+    // Example iPad simulator config:
+    // browsers: ['iPad'],
+    // customLaunchers: {
+    //   iPad: {
+    //     base: 'iOS', 
+    //     family: 'iPad'
+    //   }
+    // },
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+    // Not required if you've installed karma-ios-launcher via npm.
+    plugins: ['karma-*', require('../')]
+  });
+};
---
+++ example/test/test.js
@@ -0,0 +1,5 @@
+describe('this example', function() {
+  it('should be ok', function() { 
+    expect(1).toEqual(1);
+  });
+});
